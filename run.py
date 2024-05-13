cc0363c42772eb0897a2eea6eb15c9045d21a0a7
Anna Afijan <ann.afijan@gmail.com>
2024-05-13 18:40:26 +0000

    create color py and add color_list to py

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from colors import color_codes, MAX_TRIES
+
+
+#ANSI color escape codes
+color_codes = {
+    "red": "\033[91m",
+    "green": "\033[92m",
+    "yellow": "\033[93m",
+    "blue": "\033[94m",
+    "purple": "\033[95m",
+    "teal": "\033[96m",
+    "white": "\033[97m",
+    "orange": "\033[38;5;208m",  # Orange color
+    "maroon": "\033[38;5;88m",   # Maroon color
+    "olive": "\033[38;5;58m",    # Olive color
+    "navy": "\033[38;5;21m",     # Navy color
+    "forest green": "\033[38;5;28m",  # Forest Green color
+    "burnt umber": "\033[38;5;88m",   # Burnt Umber color
+    "burnt sienna": "\033[38;5;130m", # Burnt Sienna color
+    "goldenrod": "\033[38;5;136m",    # Goldenrod color
+    "amber": "\033[38;5;202m"         # Amber color
+}
