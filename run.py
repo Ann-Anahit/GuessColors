1d5561811c8933f678f77c0f2414ccd3d5065cf4
Anna Afijan <ann.afijan@gmail.com>
2024-05-13 19:40:06 +0000

    add list of colors

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 import random
-from colors import color_codes, MAX_TRIES
 
 
 #ANSI color escape codes
@@ -20,6 +19,31 @@ color_codes = {
     "burnt sienna": "\033[38;5;130m", # Burnt Sienna color
     "goldenrod": "\033[38;5;136m",    # Goldenrod color
     "amber": "\033[38;5;202m"         # Amber color
+
+}
+
+# The list of colors was created using ChatGPT 
+
+# List of colors
+colors = list(color_codes.keys())
+
+# Dictionary mapping color combinations to the resulting color
+color_combinations = {
+    ("red", "blue"): "purple",
+    ("blue", "yellow"): "green",
+    ("yellow", "red"): "orange",
+    ("red", "green"): "brown",
+    ("blue", "green"): "teal",
+    ("yellow", "green"): "lime",
+    ("red", "orange"): "maroon",
+    ("blue", "orange"): "burnt sienna",
+    ("yellow", "orange"): "amber",
+    ("purple", "green"): "olive",
+    ("purple", "orange"): "bronze",
+    ("purple", "yellow"): "goldenrod",
+    ("blue", "indigo"): "navy",
+    ("green", "indigo"): "forest green",
+    ("orange", "indigo"): "burnt umber"
 }
 
 def mix_colors(color1, color2):
@@ -57,3 +81,5 @@ def main():
         else:
             print("Sorry, that's not correct. The resulting color is {}.".format(color_codes[result] + result + "\033[0m"))
 
+if __name__ == "__main__":
+    main()
