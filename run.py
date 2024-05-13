4ab7cacbd7194a6cc03c9e42bea1fcc28824d587
Anna Afijan <ann.afijan@gmail.com>
2024-05-13 22:05:57 +0000

    fix indentation in the code

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 import random
 
-
-#ANSI color escape codes
+# ANSI color escape codes
 color_codes = {
     "red": "\033[91m",
     "green": "\033[92m",
@@ -19,10 +18,7 @@ color_codes = {
     "burnt sienna": "\033[38;5;130m", # Burnt Sienna color
     "goldenrod": "\033[38;5;136m",    # Goldenrod color
     "amber": "\033[38;5;202m"         # Amber color
-
 }
-
-# The list of colors was created using ChatGPT 
 
 # List of colors
 colors = list(color_codes.keys())
@@ -61,36 +57,37 @@ def main():
     print("Welcome to the Color Mixing Game!")
     print("Try to guess the resulting color when you mix two colors.")
 
-play_again = 'y'
-while play_again.lower() == 'y':
-    # Initialize guess counter
+    play_again = 'y'
+    while play_again.lower() == 'y':
+        # Initialize guess counter
         guess_count = 0
 
-    # Infinite loop for a single game
-    while guess_count < 6:
-        # Pick two random colors
-        color1 = random.choice(colors)
-        color2 = random.choice(colors)
+        # Infinite loop for a single game
+        while guess_count < 6:
+            # Pick two random colors
+            color1 = random.choice(colors)
+            color2 = random.choice(colors)
 
-        print("You mix {} and {}...".format(color_codes[color1] + color1 + "\033[0m", color_codes[color2] + color2 + "\033[0m"))
-        guess = input("What color do you think you'll get? (Type 'exit' to quit): ").lower()
+            print("You mix {} and {}...".format(color_codes[color1] + color1 + "\033[0m", color_codes[color2] + color2 + "\033[0m"))
+            guess = input("What color do you think you'll get? (Type 'exit' to quit): ").lower()
 
-        if guess == "exit":
-            print("Thanks for playing!")
-            break
+            if guess == "exit":
+                print("Thanks for playing!")
+                return
 
-        result = mix_colors(color1, color2)
+            result = mix_colors(color1, color2)
 
-        if guess == result:
-            print("Congratulations! You guessed it right. {} is the resulting color!".format(color_codes[result] + result + "\033[0m"))
-        else:
-            print("Sorry, that's not correct. The resulting color is {}.".format(color_codes[result] + result + "\033[0m"))
+            if guess == result:
+                print("Congratulations! You guessed it right. {} is the resulting color!".format(color_codes[result] + result + "\033[0m"))
+                break  # Exit the inner loop if the guess is correct
+            else:
+                print("Sorry, that's not correct. The resulting color is {}.".format(color_codes[result] + result + "\033[0m"))
+            
+            guess_count += 1
 
-        guess_count += 1
+        play_again = input("Do you want to play again? (y/n): ")
 
-       play_again = input("Do you want to play again? (y/n): ")
+    print("Thank you for playing! See you soon!")
 
-    print("Thank you for playing! See you soon!") 
-           
 if __name__ == "__main__":
     main()
